95912ec2ffc3ee2d30a6f4b28f32a604c177e187
Wataru <xbee@dream.jp>
2017-06-17 13:35:20 +0900

    液晶のハードウェアリセット機能の追加(GPIO24)

--- run_lcd.sh
+++ run_lcd.sh
@@ -2,5 +2,5 @@
 kill `pidof -x sem_com.py` &> /dev/null
 sleep 3
 #nohup ./sem_com.py < /dev/null > /dev/null 2> error.log &
-nohup ./sem_com.py < /dev/null 2> error.log | /home/pi/RaspberryPi/gpio/raspi_lcd -f &
+nohup ./sem_com.py < /dev/null 2> error.log | /home/pi/RaspberryPi/gpio/raspi_lcd -f -r24 &
 echo "done"
