0a950df3b5f5aea61074df1eff95eccfa822cd88
Wataru KUNINO <xbee@dream.jp>
2017-08-17 23:42:27 +0900

    ブロードキャストアドレスへ変更

--- user_conf.py
+++ user_conf.py
@@ -11,5 +11,5 @@ SEM_ROUTEB_ID = '00000000000000000000000000000000'
 SEM_PASSWORD = 'XXXXXXXXXXXX'
 SEM_INTERVAL = 20	# 瞬時電力取得間隔[s]
 SEM_DURATION = 6	# アクティブスキャンduration (通常は変更の必要なし)
-SOCK_UDP = '192.168.0.128'  # UDPソケットIPアドレス
+SOCK_UDP = '192.168.0.255'  # UDPソケットIPアドレス
 SOCK_PORT = 1024            # UDPポート番号（0でOFF）
