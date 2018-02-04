abe17e9c9202fa5b7d50960a40a532eb67c58b90
Wataru <xbee@dream.jp>
2018-02-04 11:55:50 +0900

    LCD表示機能の修正

---
+++ raspi_lcd.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Apple Pi DEMO
+#
+# Copyright (c) 2017 Wataru KUNINO
+#
+# /etc/rc.localへ下記を追加すると自動的に起動する
+#       /home/pi/RaspberryPi/gpio/apple_pi.sh &
+
+/home/pi/RaspberryPi/gpio/raspi_lcd -i -r24 `hostname -I|cut -d" " -f1`
+
+date > raspi_lcd.log
+next_time=$(( SECONDS + 600 ))
+while true; do
+	LCD=`tail -1 sem_com.log`
+	if [ -n "${LCD}" ]; then
+		if [ $SECONDS -gt $next_time ]; then
+			/home/pi/RaspberryPi/gpio/raspi_lcd -f -r24 "${LCD}" &>> raspi_lcd.log &
+			next_time=$(( SECONDS + 600 ))
+		else
+			/home/pi/RaspberryPi/gpio/raspi_lcd -f "${LCD}" &>> raspi_lcd.log &
+		fi
+	fi
+	sleep 6
+done
--- run_lcd.sh
+++ run_lcd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo kill `pidof -x sem_com.py` &> /dev/null
+sudo kill `pidof -x raspi_lcd.sh` &> /dev/null
 sleep 3
-nohup ./sem_com.py 2> error.log | tee sem_com.log | /home/pi/RaspberryPi/gpio/raspi_lcd -f -r24 &> lcd.log &
+nohup ./sem_com.py 2> error.log > sem_com.log &
+nohup ./raspi_lcd.sh &> raspi_lcd.log &
 echo "done"
